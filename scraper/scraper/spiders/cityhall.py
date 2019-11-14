3a3227ca2bdc91ea70691ecbce46cceac2677a0d
Ana Paula Gomes <apgomes88@gmail.com>
2019-11-14 20:10:36 +0100

    Melhorias na legibilidade do código

--- scraper/scraper/spiders/cityhall.py
+++ scraper/scraper/spiders/cityhall.py
@@ -2,7 +2,7 @@ import re
 import scrapy
 
 
-class Laws(scrapy.Spider):
+class LawsSpider(scrapy.Spider):
     """Parse laws and acts of Feira de Santana city hall until 2015.
 
     Years: 1999 to 2015
@@ -24,73 +24,81 @@ class Laws(scrapy.Spider):
         @returns items 8 13
         @returns requests 1 2
         """
-        if 'SEM INFORMA' not in response.text:  # page found
+        if 'SEM INFORMA' not in response.text:  # it means page found
             rows = response.css('table tr td table tr td table tr td table tr td.txt')
             for row in rows:
-                act_url = row.xpath('a//@href').extract_first()
-                change = row.xpath('a//text()').extract_first()
-                text = row.xpath('span/strong/text()').extract()
-                act_and_date = text[0]
-                if len(text) > 1:
-                    journal_label = text[1]  # Jornal Publicado:
-                    date = text[2]  # Data:
-
-                    whole_text = ''.join([
-                        chunk.strip().replace('-', '')
-                        for chunk in row.css('::text').extract()
-                    ])
-
-                    whole_text = whole_text \
-                        .replace(text[0], '') \
-                        .replace(change, '') \
-                        .replace(journal_label, '')
-                    date_index = whole_text.rfind(date)
-                    journal = whole_text[:date_index].strip()
-                    date = whole_text[date_index + len(date):]
-                else:
-                    journal = ''
-                    date = ''
-
+                act = self.extract_acts(row)
                 yield {
-                    'titulo': act_and_date,
-                    'jornal_publicado': journal,
-                    'data': date,
-                    'detalhes': change,
-                    'url': act_url,
+                    'titulo': act['act_and_date'],
+                    'jornal_publicado': act['journal'],
+                    'data': act['date'],
+                    'detalhes': act['details'],
+                    'url': act['url'],
                     'fonte': response.url,
                 }
 
+            current_page = self.get_current_page(response)
             last_page = response.xpath('//table/tr[10]/td/ul/li/a/text()')
-            current_page = response.xpath("//table/tr[10]/td/ul/li[contains(@class, 'current')]/a/text()").get()
-            if current_page is None:
-                current_page = response.css('li.current ::text').get()
-            if current_page is None:
-                current_page = response.css('ul.pagination li.active ::text').get()
 
             if current_page and last_page:
-                last_page = last_page[-1].get()
-                last_page = int(last_page.strip())
-                current_page = int(current_page.strip())
-                next_page = int(current_page) + 1
-
-                self.logger.info(f'=============================== next page {next_page}')
+                last_page = int(last_page[-1].get().strip())
+                next_page = int(current_page.strip()) + 1
 
                 if next_page <= last_page:
-                    page_index = response.url.find('&p=') + 3
-                    end_page_index = response.url[page_index:].find('&')
-                    before_page = response.url[:page_index]
-                    after_page = response.url[page_index:][end_page_index:]
-
-                    url = f'{before_page}{next_page}{after_page}'
+                    url = self.build_url(response.url, next_page)
                     yield response.follow(url, callback=self.parse)
         else:
-            self.logger.info(f'=============================== not found')
+            self.logger.info(f'End of pages')
+
+    def build_url(self, url, next_page):
+        page_index = url.find('&p=') + 3
+        end_page_index = url[page_index:].find('&')
+        prefix = url[:page_index]
+        params = url[page_index:][end_page_index:]
+        return f'{prefix}{next_page}{params}'
+
+    def get_current_page(self, response):
+        current_page = response.xpath(
+            "//table/tr[10]/td/ul/li[contains(@class, 'current')]/a/text()"
+        ).get()
+        if current_page is None:
+            current_page = response.css('li.current ::text').get()
+        if current_page is None:
+            current_page = response.css('ul.pagination li.active ::text').get()
+        return current_page
+
+    def extract_acts(self, row):
+        text = row.xpath('span/strong/text()').extract()
+        act = {
+            'url': row.xpath('a//@href').extract_first(),
+            'act_and_date': text[0],
+            'details': row.xpath('a//text()').extract_first(),
+            'journal': '',
+            'date': ''
+        }
+        if len(text) > 1:
+            journal_label = text[1]  # Jornal Publicado:
+            date_label = text[2]  # Data:
+
+            whole_text = ''.join([
+                chunk.strip().replace('-', '')
+                for chunk in row.css('::text').extract()
+            ])
+
+            whole_text = whole_text \
+                .replace(text[0], '') \
+                .replace(act['details'], '') \
+                .replace(journal_label, '')
+            date_index = whole_text.rfind(date_label)
+            act['journal'] = whole_text[:date_index].strip()
+            act['date'] = whole_text[date_index + len(date_label):]
+
+        return act
 
 
 class BidsSpider(scrapy.Spider):
     name = 'bids'
     start_urls = ['http://www.feiradesantana.ba.gov.br/seadm/licitacoes.asp']
-    bid_id = 0
 
     def parse(self, response):
         """
@@ -98,10 +106,10 @@ class BidsSpider(scrapy.Spider):
         @returns items 0
         @returns requests 166
         """
-        all_bidding_urls = response.xpath('//table/tbody/tr/td[1]/div/a//@href').extract()
+        urls = response.xpath('//table/tbody/tr/td[1]/div/a//@href').extract()
         base_url = 'http://www.feiradesantana.ba.gov.br'
 
-        for url in all_bidding_urls:
+        for url in urls:
             if base_url not in url:
                 # all years except 2017 and 2018
                 url = f'{base_url}/seadm/{url}'
@@ -120,28 +128,26 @@ class BidsSpider(scrapy.Spider):
         bids_history = self._parse_bids_history(raw_bids_history)
         modalities = self._parse_modalities(raw_modalities)
         when = self._parse_when(raw_when)
+        bid_data = zip(modalities, descriptions, bids_history, when)
 
-        for modality, (description, document_url), bid_history, when in zip(
-            modalities, descriptions, bids_history, when
-        ):
-            url_pattern = re.compile(r'licitacoes_pm\.asp[\?|&]cat=(\w+)\&dt=(\d+-\d+)')
+        url_pattern = re.compile(
+            r'licitacoes_pm\.asp[\?|&]cat=(\w+)\&dt=(\d+-\d+)'
+        )
+        for modality, (description, document_url), history, when in bid_data:
             match = url_pattern.search(response._url)
             month, year = match.group(2).split('-')
 
             yield {
-                'id': self.bid_id,
                 'url': response._url,
                 'category': match.group(1).upper(),
                 'month': int(month),
                 'year': int(year),
                 'description': description,
-                'history': bid_history,
+                'history': history,
                 'modality': modality,
                 'when': when,
                 'document_url': document_url,
             }
-
-            self.bid_id += 1
 
     def _parse_descriptions(self, raw_descriptions):
         descriptions = []
@@ -162,12 +168,15 @@ class BidsSpider(scrapy.Spider):
             for row in raw_bid_history.xpath('.//tr'):
                 when = row.xpath('.//td[2]/text()').get().strip()
                 event = row.xpath('.//td[3]/div/text()').get()
-                url = row.xpath('.//td[4]/div/a//@href').get()  # FIXME adopt .get(default='')
+                url = row.xpath('.//td[4]/div/a//@href').get()
 
                 if event and when:
-                    bids_history.append(
-                        {'when': when, 'event': event.capitalize(), 'url': url if url else ''}
-                    )
+                    url = url if url else ''
+                    bids_history.append({
+                        'when': when,
+                        'event': event.capitalize(),
+                        'url': url
+                    })
             all_bids_history.append(bids_history)
 
         return all_bids_history
