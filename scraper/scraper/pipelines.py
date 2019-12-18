fc5d38e7963025e989f00c3b3e5f5ab13311863e
Ana Paula Gomes <apgomes88@gmail.com>
2019-12-17 23:17:23 -0300

    Corrige nome do arquivo com query params

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -1,14 +1,35 @@
 from scraper.settings import FILES_STORE, KEEP_FILES
 import os
 
+import hashlib
+from scrapy.utils.python import to_bytes
+from six.moves.urllib.parse import urlparse
+
 from scrapy.pipelines.files import FilesPipeline
 from tika import parser
 
 
 class ExtractPDFContentPipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        """Retorna onde o arquivo foi baixado.
+
+        Copiado de https://github.com/okfn-brasil/diario-oficial/
+        Issue no scrapy: https://github.com/scrapy/scrapy/issues/4225
+
+        de:
+        8e61990b27c6158edaaa715ea76eca65459d92f4.asp?cat=PMFS&dt=03-2016
+        para:
+        8e61990b27c6158edaaa715ea76eca65459d92f4.asp
+        """
+        url = request.url
+        media_guid = hashlib.sha1(to_bytes(url)).hexdigest()
+        media_ext = os.path.splitext(url)[1]
+        if not media_ext.isalnum():
+            media_ext = os.path.splitext(urlparse(url).path)[1]
+        return "full/%s%s" % (media_guid, media_ext)
+
     def item_completed(self, results, item, info):
-        if results:
-            # FIXME supports only one file by now
+        if results and results[0][0]:
             file_info = results[0][1]
             file_path = f"{FILES_STORE}{file_info['path']}"
             raw = parser.from_file(file_path)
