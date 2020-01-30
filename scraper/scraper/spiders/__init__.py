8385fcb6316a80338e329d7782d46a0a8d9624dd
Ana Paula Gomes <apgomes88@gmail.com>
2020-01-29 22:03:32 -0300

    Add notificação de erros no Telegram (#42)
    * Adiciona monitores básicos e config do Telegram
    
    * Corrige casos onde spiders não tem data inicial
    
    * Add .zip

--- scraper/scraper/spiders/__init__.py
+++ scraper/scraper/spiders/__init__.py
@@ -8,13 +8,14 @@ class BaseSpider(scrapy.Spider):
 
     @property
     def start_date(self):
+        picked_date = None
         if self.start_from_date:
             if isinstance(self.start_from_date, str):
                 picked_date = datetime.strptime(self.start_from_date, "%d/%m/%Y")
                 picked_date = picked_date.date()
             else:
                 picked_date = self.start_from_date
-        else:
+        elif hasattr(self, "initial_date"):
             picked_date = self.initial_date
 
         return picked_date
