1030ff6fb5391c3542b6a991e666faafbe5a713d
Ana Paula Gomes <apgomes88@gmail.com>
2020-02-24 17:06:30 +0100

    Adota django-configurations

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,14 +6,10 @@ import sys
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+
+    from configurations.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
 
 
