c9b6ed15dcc2fba096be8f09991d51a6147e4382
jroquejr <rokrjr@gmail.com>
2020-03-28 18:26:46 -0300

    Auto load .env files from root path

--- manage.py
+++ manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+import env_file
+
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+
+    env_file.load()
 
     from configurations.management import execute_from_command_line
 
