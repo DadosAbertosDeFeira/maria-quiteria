2b459aee10e8f5e1cba601f026a3ebc41bf110e6
Matheus Cardoso <matheus.mcas@gmail.com>
2020-04-05 10:01:26 -0300

    Ajuste erros do flake/black

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'home/index.html', {})
+    return render(request, "home/index.html", {})
