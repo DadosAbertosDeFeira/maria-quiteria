4746be0cb165275a698dff8ec24f23122e382df4
Matheus Cardoso <matheus.mcas@gmail.com>
2020-04-05 10:08:33 -0300

    Faça a vonta do black por strs de aspas duplas 😒

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('home.urls')),
+    path("", include("home.urls")),
 ]
 
 if settings.DEBUG:
