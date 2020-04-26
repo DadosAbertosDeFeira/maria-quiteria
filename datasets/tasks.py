1bb81254454f6490fbdeb55d436b3b33d1abc4cc
Ana Paula Gomes <apgomes88@gmail.com>
2020-04-26 11:53:56 +0200

    Cria task que sobe arquivo para o S3

--- datasets/tasks.py
+++ datasets/tasks.py
@@ -1,6 +1,9 @@
+import os
 from logging import info
 from pathlib import Path
+from urllib.request import urlopen
 
+import boto3
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from dramatiq import actor, set_broker
@@ -9,15 +12,14 @@ from tika import parser
 
 # Esse bloco (feio) faz com que esse módulo funcione dentro ou fora do Django
 try:
-    from datasets.models import Gazette, CityCouncilMinute, CityHallBid
+    from datasets.models import Gazette, CityCouncilMinute, CityHallBid, File
 except ImproperlyConfigured:
-    import os
     import configurations
 
     os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     configurations.setup()
-    from datasets.models import Gazette, CityCouncilMinute, CityHallBid
+    from datasets.models import Gazette, CityCouncilMinute, CityHallBid, File
 
 
 rabbitmq_broker = RabbitmqBroker(url=settings.CLOUDAMQP_URL)
@@ -28,6 +30,12 @@ ITEM_TO_MODEL = {
     "CityCouncilMinuteItem": CityCouncilMinute,
     "CityHallBidItem": CityHallBid,
 }
+
+AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET", "test-bucket")
+AWS_S3_BUCKET_FOLDER = os.getenv("AWS_S3_BUCKET_FOLDER", "maria-quiteria-staging")
+AWS_S3_REGION = os.getenv("AWS_S3_REGION", "test-region")
+
+s3_resource = boto3.resource("s3")
 
 
 @actor
@@ -43,9 +51,48 @@ def content_from_file(item_name, url, path, checksum, save_to_db, keep_file):
             info(f"No model defined for {item_name}.")
             return
         qs = Model.objects.filter(file_url=url, file_content__isnull=True)
-        qs.update(file_content=raw["content"], checksum=checksum)
+        qs.update(file_content=raw["content"])
 
     if not keep_file:
         Path(path).unlink()
 
     return raw["content"]
+
+
+def create_temp_file(url):
+    content = urlopen(url).read()
+    start_index = url.rfind("/") + 1
+    temp_file_name = f"{url[start_index:]}"
+    open(f"{Path.cwd()}/{temp_file_name}", "wb").write(content)
+    return temp_file_name
+
+
+def delete_temp_file(temp_file_name):
+    Path(f"{Path.cwd()}/{temp_file_name}").unlink()
+
+
+@actor
+def backup_file(file_id):
+    try:
+        file_obj = File.objects.get(pk=file_id, s3_url__isnull=True)
+    except File.DoesNotExist:
+        info(f"O arquivo não existe ou já possui backup {file_id}.")
+        return
+
+    model_name = file_obj.content_object._meta.model_name
+    temp_file_name = create_temp_file(file_obj.url)
+    file_path = (
+        f"{AWS_S3_BUCKET_FOLDER}/files/{model_name}/{file_obj.created_at.year}/"
+        f"{file_obj.created_at.month}/{file_obj.created_at.day}/"
+        f"{file_obj.checksum}-{temp_file_name}"
+    )
+
+    s3_resource.Object(AWS_S3_BUCKET, file_path).upload_file(Filename=temp_file_name)
+
+    s3_url = f"https://{AWS_S3_BUCKET}.s3.{AWS_S3_REGION}.amazonaws.com/" f"{file_path}"
+    file_obj.s3_url = s3_url
+    file_obj.save()
+
+    delete_temp_file(temp_file_name)
+
+    return s3_url
--- datasets/tests/test_tasks.py
+++ datasets/tests/test_tasks.py
@@ -1,8 +1,14 @@
-import pytest
-from model_bakery import baker
+from pathlib import Path
 
+import pytest
 from datasets.models import Gazette
-from datasets.tasks import content_from_file
+from datasets.tasks import (
+    backup_file,
+    content_from_file,
+    create_temp_file,
+    delete_temp_file,
+)
+from model_bakery import baker
 
 
 def test_content_from_files_not_saving_to_db(mocker):
@@ -33,5 +39,55 @@ def test_content_from_files_saving_to_db(mocker):
     parser.from_file.assert_called_once_with(path)
     path.return_value.unlink.assert_not_called()
     assert Gazette.objects.get(file_url=url).file_content == "quarenta e dois"
-    assert Gazette.objects.get(file_url=url).checksum == "f42"
     assert result == "quarenta e dois"
+
+
+@pytest.mark.django_db
+def test_backup_file(mocker):
+    url = "http://www.feiradesantana.ba.gov.br/licitacoes/4914pmfspp2182019.pdf"
+    gazette = baker.make("datasets.Gazette")
+    a_file = baker.make(
+        "datasets.File", url=url, content_object=gazette, checksum="random"
+    )
+
+    expected_s3_url = (
+        "https://test-bucket.s3.test-region.amazonaws.com/maria-quiteria-staging/"
+        "files/gazette/2020/4/26/random-4914pmfspp2182019.pdf"
+    )
+    boto3_mock = mocker.patch("datasets.tasks.s3_resource.Object")
+
+    backup_file(a_file.pk)
+
+    assert boto3_mock.called
+    a_file.refresh_from_db()
+
+    assert a_file.s3_url == expected_s3_url
+
+
+@pytest.mark.django_db
+def test_return_none_when_file_does_not_exist():
+    assert backup_file(9) is None
+
+
+@pytest.mark.django_db
+def test_return_none_when_file_has_backup_already():
+    gazette = baker.make("datasets.Gazette")
+    a_file = baker.make(
+        "datasets.File",
+        url="https://url.com",
+        content_object=gazette,
+        checksum="random",
+        s3_url="https://s3url.com",
+    )
+    assert backup_file(a_file.pk) is None
+
+
+def test_create_temp_file():
+    url = "http://www.feiradesantana.ba.gov.br/licitacoes/4914pmfspp2182019.pdf"
+    temp_file_name = create_temp_file(url)
+
+    assert temp_file_name == "4914pmfspp2182019.pdf"
+    assert Path(f"{Path.cwd()}/4914pmfspp2182019.pdf").is_file() is True
+
+    delete_temp_file("4914pmfspp2182019.pdf")
+    assert Path(f"{Path.cwd()}/4914pmfspp2182019.pdf").is_file() is False
