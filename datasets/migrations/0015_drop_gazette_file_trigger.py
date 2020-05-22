ca3db645bc9a2d44d063a467d6ff047b732f90ff
Ana Paula Gomes <apgomes88@gmail.com>
2020-05-22 11:49:55 +0200

    Migration para remover trigger e campos

---
+++ datasets/migrations/0015_drop_gazette_file_trigger.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("datasets", "0014_citycouncilbid"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="DROP TRIGGER IF EXISTS search_vector_update ON datasets_gazette;",
+            reverse_sql="""
+                CREATE TRIGGER search_vector_update BEFORE INSERT OR UPDATE
+                ON datasets_gazette FOR EACH ROW EXECUTE PROCEDURE
+                tsvector_update_trigger(search_vector, 'pg_catalog.portuguese', file_content);
+            """,
+        ),
+    ]
