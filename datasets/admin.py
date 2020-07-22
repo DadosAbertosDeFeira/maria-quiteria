f77168156540d0ef156c4ed376ae1273ab34a847
Raul Santos <14143674+RaulBSantos@users.noreply.github.com>
2020-07-22 18:42:42 -0300

    Melhora performance das buscas (#156)

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.postgres.search import SearchQuery, SearchRank
-from django.db.models import F
+from django.contrib.postgres.search import SearchQuery
 from django.utils.safestring import mark_safe
 from public_admin.admin import PublicModelAdmin
 from public_admin.sites import PublicAdminSite, PublicApp
@@ -20,7 +19,7 @@ from .models import (
 
 
 class GazetteAdmin(PublicModelAdmin):
-    search_fields = ["year_and_edition", "files__content"]
+    search_fields = ["year_and_edition", "events__summary", "files__search_vector"]
     list_filter = ["power", "date"]
     list_display = (
         "date",
@@ -33,7 +32,21 @@ class GazetteAdmin(PublicModelAdmin):
     )
 
     def get_queryset(self, request):
-        return super().get_queryset(request).prefetch_related("files", "events")
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related("files", "events")
+            .defer("files__content")
+        )
+
+    def get_search_results(self, request, queryset, search_term):
+        if not search_term:
+            return super().get_search_results(request, queryset, search_term)
+
+        query = SearchQuery(search_term, config="portuguese")
+        queryset = queryset.filter(files__search_vector=query)
+
+        return queryset, False
 
     @mark_safe
     def events(self, obj):
@@ -128,7 +141,7 @@ class CityCouncilExpenseAdmin(PublicModelAdmin):
 
 
 class CityCouncilMinuteAdmin(PublicModelAdmin):
-    search_fields = ["title", "files__content"]
+    search_fields = ["title", "files__search_vector"]
     list_filter = ["date", "event_type"]
     list_display = (
         "date",
@@ -140,7 +153,21 @@ class CityCouncilMinuteAdmin(PublicModelAdmin):
     )
 
     def get_queryset(self, request):
-        return super().get_queryset(request).prefetch_related("files")
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related("files")
+            .defer("files__content")
+        )
+
+    def get_search_results(self, request, queryset, search_term):
+        if not search_term:
+            return super().get_search_results(request, queryset, search_term)
+
+        query = SearchQuery(search_term, config="portuguese")
+        queryset = queryset.filter(files__search_vector=query)
+
+        return queryset, False
 
     @mark_safe
     def files(self, obj):
@@ -152,7 +179,7 @@ class CityCouncilMinuteAdmin(PublicModelAdmin):
 
 
 class CityHallBidAdmin(PublicModelAdmin):
-    search_fields = ["description", "codes", "files__content"]
+    search_fields = ["description", "codes", "files__search_vector"]
     list_filter = ["session_at", "public_agency", "modality"]
     list_display = (
         "session_at",
@@ -165,7 +192,21 @@ class CityHallBidAdmin(PublicModelAdmin):
     )
 
     def get_queryset(self, request):
-        return super().get_queryset(request).prefetch_related("files", "events")
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related("files", "events", "events__files")
+            .defer("files__content", "events__files__content")
+        )
+
+    def get_search_results(self, request, queryset, search_term):
+        if not search_term:
+            return super().get_search_results(request, queryset, search_term)
+
+        query = SearchQuery(search_term, config="portuguese")
+        queryset = queryset.filter(files__search_vector=query)
+
+        return queryset, False
 
     @mark_safe
     def files(self, obj):
@@ -213,12 +254,7 @@ class FileAdmin(admin.ModelAdmin):
             return super().get_search_results(request, queryset, search_term)
 
         query = SearchQuery(search_term, config="portuguese")
-        rank = SearchRank(F("search_vector"), query)
-        queryset = (
-            File.objects.annotate(rank=rank)
-            .filter(search_vector=query)
-            .order_by("-rank")
-        )
+        queryset = queryset.filter(search_vector=query)
 
         return queryset, False
 
