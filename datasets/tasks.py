5564d9a114a0338ea5ca7d01af70a338a59a8460
Ana Paula Gomes <apgomes88@gmail.com>
2020-10-20 11:43:47 +0200

    Torna métodos em tarefas backgrounds

--- datasets/tasks.py
+++ datasets/tasks.py
@@ -1,19 +1,26 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 from logging import info
 from pathlib import Path
 
 import requests
 from datasets.services import get_s3_client
 from django.conf import settings
+from django.contrib.admin.options import get_content_type_for_model
 from django.core.exceptions import ImproperlyConfigured
 from dotenv import find_dotenv, load_dotenv
-from dramatiq import actor, middleware, set_broker
+from dramatiq import actor, get_broker, set_broker
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
 from tika import parser
 
 # Esse bloco (feio) faz com que esse módulo funcione dentro ou fora do Django
 try:
-    from datasets.models import File
+    from datasets.models import (
+        CityCouncilBid,
+        CityCouncilContract,
+        CityCouncilExpense,
+        CityCouncilRevenue,
+        File,
+    )
 except ImproperlyConfigured:
     import configurations
     import os
@@ -22,27 +29,25 @@ except ImproperlyConfigured:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     load_dotenv(find_dotenv())
     configurations.setup()
-    from datasets.models import File
+    from datasets.models import (
+        CityCouncilBid,
+        CityCouncilContract,
+        CityCouncilExpense,
+        CityCouncilRevenue,
+        File,
+    )
 
 # models precisam ser importados depois das configurações
 # para manter compatibilidade com o scraper
-from datasets.webservices.citycouncil import (  # noqa isort:skip
-    add_bid,
-    add_contract,
-    add_expense,
-    remove_bid,
-    remove_contract,
-    remove_expense,
-    update_bid,
-    update_contract,
-    update_expense,
-    add_revenue,
-    update_revenue,
-    remove_revenue,
+from datasets.adapters import (  # noqa isort:skip
+    to_citycouncil_bid,
+    to_citycouncil_contract,
+    to_citycouncil_expense,
+    to_citycouncil_revenue,
 )
 
+
 rabbitmq_broker = RabbitmqBroker(url=settings.CLOUDAMQP_URL)
-rabbitmq_broker.add_middleware(middleware.Prometheus())
 set_broker(rabbitmq_broker)
 client = get_s3_client(settings)
 
@@ -125,16 +130,7 @@ def get_city_council_updates():
     return response
 
 
-@actor(max_retries=3)
-def sync_data_from_webservice(action, record):
-    """Executa uma ação a partir de um método e um registro.
-
-    A ação pode ser a inclusão de um contrato, por exemplo, e
-    o registro são os dados de um contrato novo.
-    """
-    action(record)
-
-
+@actor(max_retries=0)
 def distribute_city_council_objects_to_sync(payload):
     """Recebe o payload e dispara uma task para cada registro.
 
@@ -144,20 +140,143 @@ def distribute_city_council_objects_to_sync(payload):
     caso tenham algum erro, possam ser tratados de maneira separada.
     """
     action_methods = {
-        "inclusoesContrato": add_contract,
-        "alteracoesContrato": update_contract,
-        "exclusoesContrato": remove_contract,
-        "inclusoesLicitacao": add_bid,
-        "alteracoesLicitacao": update_bid,
-        "exclusoesLicitacao": remove_bid,
-        "inclusoesReceita": add_revenue,
-        "alteracoesReceita": update_revenue,
-        "exclusoesReceita": remove_revenue,
-        "inclusoesDespesa": add_expense,
-        "alteracoesDespesa": update_expense,
-        "exclusoesDespesa": remove_expense,
+        "inclusoesContrato": add_citycouncil_contract,
+        "alteracoesContrato": update_citycouncil_contract,
+        "exclusoesContrato": remove_citycouncil_contract,
+        "inclusoesLicitacao": add_citycouncil_bid,
+        "alteracoesLicitacao": update_citycouncil_bid,
+        "exclusoesLicitacao": remove_citycouncil_bid,
+        "inclusoesReceita": add_citycouncil_revenue,
+        "alteracoesReceita": update_citycouncil_revenue,
+        "exclusoesReceita": remove_citycouncil_revenue,
+        "inclusoesDespesa": add_citycouncil_expense,
+        "alteracoesDespesa": update_citycouncil_expense,
+        "exclusoesDespesa": remove_citycouncil_expense,
     }
+    broker = get_broker()
     for action_name, records in payload.items():
-        method = action_methods.get(action_name)
+        task = action_methods.get(action_name)
         for record in records:
-            sync_data_from_webservice.send(method, record)
+            broker.enqueue(task.message(record))
+
+
+def save_citycouncil_files(files, object, url_key):
+    if not files:
+        return
+    content_type = get_content_type_for_model(object)
+    from datasets.management.commands._file import save_file
+
+    if files:
+        for file_ in files:
+            url = f"{settings.CITY_COUNCIL_WEBSERVICE}{file_[url_key]}"
+            save_file(url, content_type, object.pk)
+
+
+@actor
+def add_citycouncil_bid(record):
+    new_item = to_citycouncil_bid(record)
+    new_item["crawled_at"] = datetime.now()
+    new_item["crawled_from"] = settings.CITY_COUNCIL_WEBSERVICE_ENDPOINT
+    bid = CityCouncilBid.objects.create(**new_item)
+    save_citycouncil_files(record.get("arquivos"), bid, "caminhoArqLic")
+
+    return bid
+
+
+@actor
+def update_citycouncil_bid(record):
+    bid = CityCouncilBid.objects.get(external_code=record["codLic"])
+    updated_item = to_citycouncil_bid(record)
+    for key, value in updated_item.items():
+        setattr(bid, key, value)
+    bid.save()
+    save_citycouncil_files(record.get("arquivos"), bid, "caminhoArqLic")
+
+    return bid
+
+
+@actor
+def remove_citycouncil_bid(record):
+    CityCouncilBid.objects.filter(external_code=record["codLic"]).update(excluded=True)
+
+
+@actor
+def add_citycouncil_contract(record):
+    new_item = to_citycouncil_contract(record)
+    new_item["crawled_at"] = datetime.now()
+    new_item["crawled_from"] = settings.CITY_COUNCIL_WEBSERVICE_ENDPOINT
+    contract = CityCouncilContract.objects.create(**new_item)
+    save_citycouncil_files(record.get("arquivos"), contract, "caminho")
+
+    return contract
+
+
+@actor
+def update_citycouncil_contract(record):
+    contract = CityCouncilContract.objects.get(external_code=record["codCon"])
+    updated_item = to_citycouncil_contract(record)
+    for key, value in updated_item.items():
+        setattr(contract, key, value)
+    contract.save()
+    save_citycouncil_files(record.get("arquivos"), contract, "caminho")
+
+    return contract
+
+
+@actor
+def remove_citycouncil_contract(record):
+    CityCouncilContract.objects.filter(external_code=record["codCon"]).update(
+        excluded=True
+    )
+
+
+@actor
+def add_citycouncil_revenue(record):
+    new_item = to_citycouncil_revenue(record)
+    new_item["crawled_at"] = datetime.now()
+    new_item["crawled_from"] = settings.CITY_COUNCIL_WEBSERVICE_ENDPOINT
+    return CityCouncilRevenue.objects.create(**new_item)
+
+
+@actor
+def update_citycouncil_revenue(record):
+    revenue = CityCouncilRevenue.objects.get(external_code=record["codLinha"])
+    updated_item = to_citycouncil_revenue(record)
+    for key, value in updated_item.items():
+        setattr(revenue, key, value)
+    revenue.save()
+    return revenue
+
+
+@actor
+def remove_citycouncil_revenue(record):
+    CityCouncilRevenue.objects.filter(external_code=record["codLinha"]).update(
+        excluded=True
+    )
+
+
+@actor
+def add_citycouncil_expense(record):
+    new_item = to_citycouncil_expense(record)
+    new_item["crawled_at"] = datetime.now()
+    new_item["crawled_from"] = settings.CITY_COUNCIL_WEBSERVICE_ENDPOINT
+    return CityCouncilExpense.objects.create(**new_item)
+
+
+@actor
+def update_citycouncil_expense(record):
+    expense = CityCouncilExpense.objects.get(
+        external_file_code=record["codArquivo"], external_file_line=record["codLinha"],
+    )
+    updated_item = to_citycouncil_expense(record)
+    for key, value in updated_item.items():
+        setattr(expense, key, value)
+    expense.save()
+    return expense
+
+
+@actor
+def remove_citycouncil_expense(record):
+    CityCouncilExpense.objects.filter(
+        external_file_code=record["codArquivo"], external_file_line=record["codLinha"],
+    ).update(excluded=True)
--- datasets/tests/test_tasks.py
+++ datasets/tests/test_tasks.py
@@ -1,11 +1,32 @@
+from datetime import date, datetime
+
 import pytest
+from datasets.models import (
+    CityCouncilBid,
+    CityCouncilContract,
+    CityCouncilExpense,
+    CityCouncilRevenue,
+)
 from datasets.tasks import (
     WebserviceException,
+    add_citycouncil_bid,
+    add_citycouncil_contract,
+    add_citycouncil_expense,
+    add_citycouncil_revenue,
     backup_file,
     content_from_file,
     distribute_city_council_objects_to_sync,
     get_city_council_updates,
+    remove_citycouncil_bid,
+    remove_citycouncil_contract,
+    remove_citycouncil_expense,
+    remove_citycouncil_revenue,
+    update_citycouncil_bid,
+    update_citycouncil_contract,
+    update_citycouncil_expense,
+    update_citycouncil_revenue,
 )
+from django.conf import settings
 from model_bakery import baker
 
 
@@ -159,14 +180,14 @@ class TestDistributeCityCouncilObjectsToSync:
             "alteracoesDespesa": [],
             "exclusoesDespesa": [],
         }
-        sync_data_task = mocker.patch("datasets.tasks.sync_data_from_webservice.send")
+        task = mocker.patch("datasets.tasks.update_citycouncil_bid.message")
+        task.return_value.queue_name = "default"
 
         distribute_city_council_objects_to_sync(payload)
 
-        assert sync_data_task.called is True
-        assert sync_data_task.call_count == 1
-        assert sync_data_task.call_args_list[0][0][0].__name__ == "update_bid"
-        assert sync_data_task.call_args_list[0][0][1] == record_data
+        assert task.called is True
+        assert task.call_count == 1
+        assert task.call_args_list[0][0][0] == record_data
 
     def test_do_not_call_task_if_there_is_no_records(self, mocker):
         payload = {
@@ -184,7 +205,432 @@ class TestDistributeCityCouncilObjectsToSync:
             "exclusoesDespesa": [],
         }
 
-        sync_data_task = mocker.patch("datasets.tasks.sync_data_from_webservice")
+        task = mocker.patch("datasets.tasks.update_citycouncil_bid.message")
+        task.return_value.queue_name = "default"
+
         distribute_city_council_objects_to_sync(payload)
 
-        assert sync_data_task.called is False
+        assert task.called is False
+
+
+@pytest.mark.django_db
+class TestCityCouncilBid:
+    def test_add_citycouncil_bid(self):
+        assert CityCouncilBid.objects.count() == 0
+
+        record = {
+            "codLic": "214",
+            "codTipoLic": "7",
+            "numLic": "004/2020",
+            "numTipoLic": "004/2020",
+            "objetoLic": "Contratação de pessoa jurídica",
+            "dtLic": "2020-03-26 09:00:00",
+            "arquivos": [],
+        }
+        bid = add_citycouncil_bid(record)
+        assert CityCouncilBid.objects.count() == 1
+        assert isinstance(bid.crawled_at, datetime)
+        assert bid.crawled_from == settings.CITY_COUNCIL_WEBSERVICE_ENDPOINT
+        assert bid.external_code == record["codLic"]
+        assert bid.modality == "pregao_presencial"
+        assert bid.code == record["numLic"]
+        assert bid.code_type == record["numTipoLic"]
+        assert bid.description == record["objetoLic"]
+        assert bid.session_at == datetime(2020, 3, 26, 9, 0, 0)
+        assert bid.excluded is False
+        assert bid.files.count() == 0
+
+    def test_add_citycouncil_bid_with_files(self, mock_save_file):
+        assert CityCouncilBid.objects.count() == 0
+
+        record = {
+            "codLic": "214",
+            "codTipoLic": "7",
+            "numLic": "004/2020",
+            "numTipoLic": "004/2020",
+            "objetoLic": "Contratação de pessoa jurídica",
+            "dtLic": "2020-03-26 09:00:00",
+            "arquivos": [
+                {
+                    "codArqLic": "1396",
+                    "codLic": "214",
+                    "dsArqLic": "publicacao.doc",
+                    "caminhoArqLic": "upload/licitacao/publicacao.doc",
+                },
+                {
+                    "codArqLic": "1397",
+                    "codLic": "214",
+                    "dsArqLic": "publicacao2.doc",
+                    "caminhoArqLic": "upload/licitacao/publicacao2.doc",
+                },
+            ],
+        }
+        bid = add_citycouncil_bid(record)
+        assert CityCouncilBid.objects.count() == 1
+        assert bid.files.count() == 2
+
+    def test_update_citycouncil_bid(self):
+        bid = baker.make_recipe("datasets.CityCouncilBid", external_code=214)
+        record = {
+            "codLic": "214",
+            "codTipoLic": "7",
+            "numLic": "004/2020",
+            "numTipoLic": "004/2020",
+            "objetoLic": "Contratação de pessoa jurídica",
+            "dtLic": "2020-03-26 09:00:00",
+            "arquivos": [],
+        }
+        updated_bid = update_citycouncil_bid(record)
+        assert bid.pk == updated_bid.pk
+        assert updated_bid.modality == "pregao_presencial"
+        assert updated_bid.code == record["numLic"]
+        assert updated_bid.code_type == record["numTipoLic"]
+        assert updated_bid.description == record["objetoLic"]
+        assert updated_bid.session_at == datetime(2020, 3, 26, 9, 0, 0)
+        assert updated_bid.excluded is False
+
+    def test_remove_citycouncil_bid(self):
+        bid = baker.make_recipe(
+            "datasets.CityCouncilBid", external_code=214, excluded=False
+        )
+        record = {"codLic": "214"}
+        remove_citycouncil_bid(record)
+
+        bid.refresh_from_db()
+        assert bid.excluded is True
+
+    def test_update_citycouncil_files(self, mock_save_file):
+        bid = baker.make_recipe("datasets.CityCouncilBid", external_code=214)
+        assert bid.files.count() == 0
+
+        record = {
+            "codLic": "214",
+            "arquivos": [
+                {
+                    "codArqLic": "1396",
+                    "codLic": "215",
+                    "dsArqLic": "publicacao.doc",
+                    "caminhoArqLic": "upload/licitacao/publicacao.doc",
+                }
+            ],
+        }
+        updated_bid = update_citycouncil_bid(record)
+        assert bid.pk == updated_bid.pk
+        assert bid.files.count() == 1
+
+
+@pytest.mark.django_db
+class TestCityCouncilContract:
+    def test_add_citycouncil_contract(self):
+        assert CityCouncilContract.objects.count() == 0
+        record = {
+            "codCon": "43",
+            "dsCon": "CONTRATO Nº 004/2014 - PRESTAÇÃO DE SERVIÇO",
+            "objetoCon": "Contratação conforme Licitação 01/2014, Pregão 01/2014.",
+            "cpfCnpjCon": "92.559.830/0001-71",
+            "nmCon": "GREEN CARD S/A REFEIÇÕES COMÉRCIO E SERVIÇOS",
+            "valorCon": "1157115,96",
+            "dtCon": "28/3/2014",
+            "dtConfim": "27/3/2015",
+            "excluido": "N",
+            "arquivos": [],
+        }
+        contract_obj = add_citycouncil_contract(record)
+
+        expected_contract = {
+            "external_code": "43",
+            "description": "CONTRATO Nº 004/2014 - PRESTAÇÃO DE SERVIÇO",
+            "details": "Contratação conforme Licitação 01/2014, Pregão 01/2014.",
+            "company_or_person_document": "92.559.830/0001-71",
+            "company_or_person": "GREEN CARD S/A REFEIÇÕES COMÉRCIO E SERVIÇOS",
+            "value": 1157115.96,
+            "start_date": date(2014, 3, 28),
+            "end_date": date(2015, 3, 27),
+            "excluded": False,
+        }
+
+        assert CityCouncilContract.objects.count() == 1
+        assert contract_obj.external_code == expected_contract["external_code"]
+        assert contract_obj.description == expected_contract["description"]
+        assert contract_obj.details == expected_contract["details"]
+        assert (
+            contract_obj.company_or_person_document
+            == expected_contract["company_or_person_document"]
+        )
+        assert contract_obj.company_or_person == expected_contract["company_or_person"]
+        assert contract_obj.value == expected_contract["value"]
+        assert contract_obj.start_date == expected_contract["start_date"]
+        assert contract_obj.end_date == expected_contract["end_date"]
+        assert contract_obj.excluded == expected_contract["excluded"]
+
+    def test_add_citycouncil_contract_with_files(self, mock_save_file):
+        assert CityCouncilContract.objects.count() == 0
+        record = {
+            "codCon": "43",
+            "dsCon": "CONTRATO Nº 004/2014 - PRESTAÇÃO DE SERVIÇO",
+            "objetoCon": "Contratação conforme Licitação 01/2014, Pregão 01/2014.",
+            "cpfCnpjCon": "92.559.830/0001-71",
+            "nmCon": "GREEN CARD S/A REFEIÇÕES COMÉRCIO E SERVIÇOS",
+            "valorCon": "1157115,96",
+            "dtCon": "28/3/2014",
+            "dtConfim": "27/3/2015",
+            "excluido": "N",
+            "arquivos": [
+                {
+                    "codArqCon": "1396",
+                    "codCon": "43",
+                    "dsArqCon": "publicacao.doc",
+                    "caminho": "upload/publicacao.doc",
+                }
+            ],
+        }
+        contract_obj = add_citycouncil_contract(record)
+
+        assert CityCouncilContract.objects.count() == 1
+        assert contract_obj.files.count() == 1
+
+    def test_update_citycouncil_contract(self):
+        contract = baker.make_recipe("datasets.CityCouncilContract", external_code=43)
+        record = {
+            "codCon": "43",
+            "dsCon": "CONTRATO Nº 004/2014 - PRESTAÇÃO DE SERVIÇO",
+            "objetoCon": "Contratação conforme Licitação 01/2014, Pregão 01/2014.",
+            "cpfCnpjCon": "92.559.830/0001-71",
+            "nmCon": "GREEN CARD S/A REFEIÇÕES COMÉRCIO E SERVIÇOS",
+            "valorCon": "1157115,96",
+            "dtCon": "28/3/2014",
+            "dtConfim": "27/3/2015",
+            "excluido": "N",
+            "arquivos": [],
+        }
+        updated_contract = update_citycouncil_contract(record)
+
+        assert contract.pk == updated_contract.pk
+
+    def test_update_citycouncil_contract_with_files(self, mock_save_file):
+        contract = baker.make_recipe("datasets.CityCouncilContract", external_code=43)
+        assert contract.files.count() == 0
+        record = {
+            "codCon": "43",
+            "arquivos": [
+                {
+                    "codArqCon": "1396",
+                    "codCon": "43",
+                    "dsArqCon": "publicacao.doc",
+                    "caminho": "upload/publicacao.doc",
+                }
+            ],
+        }
+        updated_contract = update_citycouncil_contract(record)
+
+        assert contract.pk == updated_contract.pk
+        assert updated_contract.files.count() == 1
+
+    def test_remove_citycouncil_contract(self):
+        contract = baker.make_recipe(
+            "datasets.CityCouncilContract", external_code=214, excluded=False
+        )
+        record = {"codCon": "214"}
+        remove_citycouncil_contract(record)
+
+        contract.refresh_from_db()
+        assert contract.excluded is True
+
+
+@pytest.mark.django_db
+class TestCityCouncilRevenue:
+    def test_add_citycouncil_revenue(self):
+        assert CityCouncilRevenue.objects.count() == 0
+        record = {
+            "codLinha": "27",
+            "codUnidGestora": "101",
+            "dtPublicacao": "1/1/2014",
+            "dtRegistro": "1/1/2014",
+            "tipoRec": "ORC",
+            "modalidade": "RECEBIMENTO",
+            "dsReceita": "Repasse Efetuado Nesta Data",
+            "valor": "1262150,07",
+            "fonte": "PREFEITURA",
+            "dsNatureza": "1.7.1.3.01.00.00 - Transferências Correntes",
+            "destinacao": "1.7.1.3.01.00.00 - Transferências Correntes",
+            "excluido": "N",
+        }
+        revenue_obj = add_citycouncil_revenue(record)
+
+        expected_revenue = {
+            "external_code": "27",
+            "budget_unit": "101",
+            "published_at": date(2014, 1, 1),
+            "registered_at": date(2014, 1, 1),
+            "revenue_type": "orcamentaria",
+            "modality": "recebimento",
+            "description": "Repasse Efetuado Nesta Data",
+            "value": 1262150.07,
+            "resource": "prefeitura",
+            "legal_status": "1.7.1.3.01.00.00 - transferências correntes",
+            "destination": "1.7.1.3.01.00.00 - transferências correntes",
+            "excluded": False,
+        }
+
+        assert CityCouncilRevenue.objects.count() == 1
+
+        assert revenue_obj.external_code == expected_revenue["external_code"]
+        assert revenue_obj.budget_unit == expected_revenue["budget_unit"]
+        assert revenue_obj.published_at == expected_revenue["published_at"]
+        assert revenue_obj.registered_at == expected_revenue["registered_at"]
+        assert revenue_obj.revenue_type == expected_revenue["revenue_type"]
+        assert revenue_obj.modality == expected_revenue["modality"]
+        assert revenue_obj.description == expected_revenue["description"]
+        assert revenue_obj.value == expected_revenue["value"]
+        assert revenue_obj.resource == expected_revenue["resource"]
+        assert revenue_obj.legal_status == expected_revenue["legal_status"]
+        assert revenue_obj.destination == expected_revenue["destination"]
+        assert revenue_obj.excluded == expected_revenue["excluded"]
+
+    def test_update_citycouncil_revenue(self):
+        revenue = baker.make_recipe("datasets.CityCouncilRevenue", external_code=43)
+        record = {
+            "codLinha": "43",
+            "codUnidGestora": "101",
+            "dtPublicacao": "1/1/2014",
+            "dtRegistro": "1/1/2014",
+            "tipoRec": "ORC",
+            "modalidade": "RECEBIMENTO",
+            "dsReceita": "Repasse Efetuado Nesta Data",
+            "valor": "1262150,07",
+            "fonte": "PREFEITURA",
+            "dsNatureza": "1.7.1.3.01.00.00 - Transferências Correntes",
+            "destinacao": "1.7.1.3.01.00.00 - Transferências Correntes",
+            "excluido": "N",
+        }
+        updated_revenue = update_citycouncil_revenue(record)
+
+        assert revenue.pk == updated_revenue.pk
+
+    def test_remove_citycouncil_revenue(self):
+        revenue = baker.make_recipe(
+            "datasets.CityCouncilRevenue", external_code=214, excluded=False
+        )
+        record = {"codLinha": "214"}
+        remove_citycouncil_revenue(record)
+
+        revenue.refresh_from_db()
+        assert revenue.excluded is True
+
+
+@pytest.mark.django_db
+class TestCityCouncilExpense:
+    def test_add_citycouncil_expense(self):
+        assert CityCouncilExpense.objects.count() == 0
+        record = {
+            "codArquivo": "253",
+            "codEtapa": "EMP",
+            "codLinha": "2",
+            "codUnidOrcam": "101",
+            "dsDespesa": "IMPORTE DESTINADO A PAGAMENTO DE SUBSIDIOS DURANTE O "
+            "PERIODO.                                         ",
+            "dsFonteRec": "0000 - " "TESOURO",
+            "dsFuncao": "01 - " "LEGISLATIVA",
+            "dsNatureza": "319011010000000000 - V.Vant.Fixas P.Civil(Ve.Base "
+            "Folha)                2003 - Administracao da acao "
+            "legislativa                          ",
+            "dsSubfuncao": "031 - " "ACAO",
+            "dtPublicacao": "2/1/2014",
+            "dtRegistro": "2/1/2014",
+            "excluido": "N",
+            "modalidade": "ISENTO        ",
+            "nmCredor": "VEREADORES      ",
+            "nuCpfCnpj": "14.488.415/0001-60",
+            "numEtapa": "14000001        ",
+            "numProcadm": "001/2014      ",
+            "numProclic": "              ",
+            "valor": "3790000,00",
+        }
+        expense_obj = add_citycouncil_expense(record)
+
+        expected_expense = {
+            "phase": "empenho",
+            "budget_unit": "101",
+            "summary": "IMPORTE DESTINADO A PAGAMENTO DE SUBSIDIOS DURANTE O PERIODO.",
+            "resource": "0000 - TESOURO",
+            "function": "01 - LEGISLATIVA",
+            "legal_status": "319011010000000000 - V.Vant.Fixas P.Civil(Ve.Base "
+            "Folha)                2003 - Administracao da acao "
+            "legislativa",
+            "subfunction": "031 - ACAO",
+            "published_at": date(2014, 1, 2),
+            "date": date(2014, 1, 2),
+            "excluded": False,
+            "modality": "isento",
+            "company_or_person": "VEREADORES",
+            "document": "14.488.415/0001-60",
+            "phase_code": "14000001",
+            "number": "001/2014",
+            "process_number": "",
+            "value": 3790000.00,
+            "external_file_code": 253,
+            "external_file_line": 2,
+        }
+
+        assert CityCouncilExpense.objects.count() == 1
+
+        assert expense_obj.phase == expected_expense["phase"]
+        assert expense_obj.budget_unit == expected_expense["budget_unit"]
+        assert expense_obj.resource == expected_expense["resource"]
+        assert expense_obj.function == expected_expense["function"]
+        assert expense_obj.legal_status == expected_expense["legal_status"]
+        assert expense_obj.subfunction == expected_expense["subfunction"]
+        assert expense_obj.published_at == expected_expense["published_at"]
+        assert expense_obj.excluded == expected_expense["excluded"]
+        assert expense_obj.modality == expected_expense["modality"]
+        assert expense_obj.company_or_person == expected_expense["company_or_person"]
+        assert expense_obj.document == expected_expense["document"]
+        assert expense_obj.phase_code == expected_expense["phase_code"]
+        assert expense_obj.number == expected_expense["number"]
+        assert expense_obj.process_number == expected_expense["process_number"]
+        assert expense_obj.value == expected_expense["value"]
+
+    def test_update_citycouncil_expense(self):
+        expense = baker.make_recipe(
+            "datasets.CityCouncilExpense", external_file_code=253, external_file_line=2,
+        )
+        record = {
+            "codArquivo": "253",
+            "codEtapa": "EMP",
+            "codLinha": "2",
+            "codUnidOrcam": "101",
+            "dsDespesa": "IMPORTE DESTINADO A PAGAMENTO DE SUBSIDIOS DURANTE O "
+            "PERIODO.                                         ",
+            "dsFonteRec": "0000 - " "TESOURO",
+            "dsFuncao": "01 - " "LEGISLATIVA",
+            "dsNatureza": "319011010000000000 - V.Vant.Fixas P.Civil(Ve.Base "
+            "Folha)                2003 - Administracao da acao "
+            "legislativa                          ",
+            "dsSubfuncao": "031 - " "ACAO",
+            "dtPublicacao": "2/1/2014",
+            "dtRegistro": "2/1/2014",
+            "excluido": "N",
+            "modalidade": "ISENTO        ",
+            "nmCredor": "VEREADORES      ",
+            "nuCpfCnpj": "14.488.415/0001-60",
+            "numEtapa": "14000001        ",
+            "numProcadm": "001/2014      ",
+            "numProclic": "              ",
+            "valor": "3790000,00",
+        }
+        updated_expense = update_citycouncil_expense(record)
+
+        assert expense.pk == updated_expense.pk
+
+    def test_remove_citycouncil_expense(self):
+        expense = baker.make_recipe(
+            "datasets.CityCouncilExpense",
+            external_file_code=253,
+            external_file_line=2,
+            excluded=False,
+        )
+        record = {"codArquivo": "253", "codLinha": "2"}
+        remove_citycouncil_expense(record)
+
+        expense.refresh_from_db()
+        assert expense.excluded is True
