c35e8e9cde2380a9db97899529a5e0857e3af3ad
Ana Paula Gomes <apgomes88@gmail.com>
2020-10-26 20:59:44 +0100

    Atualiza migrations e remove variável não utilizada

--- datasets/signals.py
+++ datasets/signals.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from dramatiq import pipeline
@@ -11,16 +10,11 @@ def backup_and_extract_content(sender, instance, **kwargs):
     from .tasks import backup_file, content_from_file
 
     if instance.s3_url is None or instance.content is None:
-        if settings.ASYNC_FILE_PROCESSING:
-            pipeline(
-                [
-                    backup_file.message(instance.pk),
-                    content_from_file.message_with_options(
-                        pipe_ignore=True, args=(instance.pk,)
-                    ),
-                ]
-            ).run()
-        else:
-            backup_file_url = backup_file(instance.pk)
-            if backup_file_url:
-                content_from_file(instance.pk)
+        pipeline(
+            [
+                backup_file.message(instance.pk),
+                content_from_file.message_with_options(
+                    pipe_ignore=True, args=(instance.pk,)
+                ),
+            ]
+        ).run()
--- datasets/tests/conftest.py
+++ datasets/tests/conftest.py
@@ -3,6 +3,5 @@ import pytest
 
 @pytest.fixture
 def mock_save_file(mocker, settings):
-    settings.ASYNC_FILE_PROCESSING = True
     pipeline_mock = mocker.patch("datasets.signals.pipeline")
     return pipeline_mock
