24ff47f7c9e4a5d6eb40cc17b6c951414f492346
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-27 09:49:15 +0100

    Black it

--- datasets/migrations/0024_auto_20201127_0518.py
+++ datasets/migrations/0024_auto_20201127_0518.py
@@ -6,30 +6,64 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datasets', '0023_auto_20201124_0458'),
+        ("datasets", "0023_auto_20201124_0458"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='SyncInformation',
+            name="SyncInformation",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criado em')),
-                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Atualizado em')),
-                ('date', models.DateField(verbose_name='Data alvo')),
-                ('source', models.CharField(choices=[('camara', 'Câmara Municipal'), ('prefeitura', 'Prefeitura')], db_index=True, max_length=20, verbose_name='Fonte')),
-                ('succeed', models.BooleanField(null=True, verbose_name='Concluída com sucesso?')),
-                ('response', models.JSONField(null=True, verbose_name='Resposta')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created_at",
+                    models.DateTimeField(auto_now_add=True, verbose_name="Criado em"),
+                ),
+                (
+                    "updated_at",
+                    models.DateTimeField(auto_now=True, verbose_name="Atualizado em"),
+                ),
+                ("date", models.DateField(verbose_name="Data alvo")),
+                (
+                    "source",
+                    models.CharField(
+                        choices=[
+                            ("camara", "Câmara Municipal"),
+                            ("prefeitura", "Prefeitura"),
+                        ],
+                        db_index=True,
+                        max_length=20,
+                        verbose_name="Fonte",
+                    ),
+                ),
+                (
+                    "succeed",
+                    models.BooleanField(
+                        null=True, verbose_name="Concluída com sucesso?"
+                    ),
+                ),
+                ("response", models.JSONField(null=True, verbose_name="Resposta")),
             ],
         ),
         migrations.AlterField(
-            model_name='citycouncilrevenue',
-            name='external_code',
-            field=models.PositiveIntegerField(db_index=True, unique=True, verbose_name='Código externo'),
+            model_name="citycouncilrevenue",
+            name="external_code",
+            field=models.PositiveIntegerField(
+                db_index=True, unique=True, verbose_name="Código externo"
+            ),
         ),
         migrations.AlterField(
-            model_name='historicalcitycouncilrevenue',
-            name='external_code',
-            field=models.PositiveIntegerField(db_index=True, verbose_name='Código externo'),
+            model_name="historicalcitycouncilrevenue",
+            name="external_code",
+            field=models.PositiveIntegerField(
+                db_index=True, verbose_name="Código externo"
+            ),
         ),
     ]
