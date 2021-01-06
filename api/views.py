204a4b0d09c20c6beb7950030868a9400fdfb395
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2021-01-05 22:52:23 -0400

    [WIP] Adiciona filtros por descricao e data ao endpoint de licitacoes da prefeitura

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import ListAPIView
-from datasets.models import CityHallBid, CityHallBidEvent
+from datasets.models import CityHallBid
 from .serializers import CityHallBidSerializer
 
 
@@ -19,14 +19,38 @@ class CityHallBidView(ListAPIView):
     serializer_class = CityHallBidSerializer
 
     def get_queryset(self):
-        bids = CityHallBid.objects.all()
-        events = CityHallBidEvent.objects.all()
+        bids = CityHallBid.objects.prefetch_related('events')
+        query_params = self.request.query_params
 
-        description = self.request.query_params.get('description', None)
+        if query_params:
+            bids = self.filter_by_query_params(bids, query_params)
 
+        return bids
+
+    @staticmethod
+    def filter_by_query_params(bids, query_params):
+        bids = CityHallBidView.filter_by_description(bids, query_params)
+        bids = CityHallBidView.filter_by_start_date(bids, query_params)
+        bids = CityHallBidView.filter_by_end_date(bids, query_params)
+        return bids
+
+    @staticmethod
+    def filter_by_end_date(bids, query_params):
+        end_date = query_params.get('end_date', None)
+        if end_date is not None:
+            bids = bids.filter(session_at__lte=end_date)
+        return bids
+
+    @staticmethod
+    def filter_by_start_date(bids, query_params):
+        start_date = query_params.get('start_date', None)
+        if start_date is not None:
+            bids = bids.filter(session_at__gte=start_date)
+        return bids
+
+    @staticmethod
+    def filter_by_description(bids, query_params):
+        description = query_params.get('query', None)
         if description is not None:
-            bids = bids.filter(description__contains=description)
-            events = events.filter(summary__contains=description)
-            bids = bids.filter(events__in=events)
-
+            bids = bids.filter(description__icontains=description) | bids.filter(events__summary__icontains=description)
         return bids
