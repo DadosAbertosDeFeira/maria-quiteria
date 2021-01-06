918db7f27d99017fe4152b17ab4484f5c1be2920
Beatriz Uezu <beatriz.uezu@gmail.com>
2021-01-06 16:49:38 -0300

    remove import nao utilizado

--- api/views.py
+++ api/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# from .filters import CityCouncilAgendaFilter
 from .serializers import CityCouncilAgendaSerializer
 
 
