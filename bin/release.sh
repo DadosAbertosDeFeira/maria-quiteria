9b7da8f40d8c1c1047fc989064529b327479e184
Ana Paula Gomes <apgomes88@gmail.com>
2021-01-10 10:49:55 +0100

    Adiciona Tika Jar

--- bin/release.sh
+++ bin/release.sh
@@ -7,7 +7,4 @@ PYTHON=$(which python3)
 echo "Running migrations"
 ${PYTHON} manage.py migrate --no-input
 
-echo "Downloading Apache Tika"
-wget https://www.apache.org/dyn/closer.cgi/tika/tika-server-1.25.jar -O tika-server.jar
-
 echo "Done!"
