cfa05ac7f80e9782eeb60927f12563e6f065396f
Francílio Araújo <francilio.araujo@pathfind.com.br>
2021-02-09 08:41:54 -0300

    Middleware já registrado. Causava erros ao iniciar

--- web/datasets/tasks.py
+++ web/datasets/tasks.py
@@ -27,7 +27,6 @@ except ImproperlyConfigured:
 
 
 rabbitmq_broker = RabbitmqBroker(url=settings.BROKER_URL)
-rabbitmq_broker.add_middleware(middleware.Prometheus())
 set_broker(rabbitmq_broker)
 client = get_s3_client(settings)
 
