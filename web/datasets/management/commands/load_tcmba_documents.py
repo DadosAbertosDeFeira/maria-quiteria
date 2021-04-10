4dfbf1bf9e13e50f198c1da469434555b0c1ec8b
Ana Paula Gomes <apgomes88@gmail.com>
2021-04-10 11:29:08 +0200

    Remove métodos não utilizados e melhora lógica para evitar duplicados

--- web/datasets/management/commands/load_tcmba_documents.py
+++ web/datasets/management/commands/load_tcmba_documents.py
@@ -1,7 +1,6 @@
 import json
 import urllib
-from datetime import datetime
-from pathlib import Path
+from datetime import datetime, timezone
 
 from django.conf import settings
 from django.contrib.admin.options import get_content_type_for_model
@@ -11,20 +10,6 @@ from web.datasets.parsers import from_str_to_date
 from web.datasets.services import get_s3_client
 
 client = get_s3_client(settings)
-
-
-def extract_params(s3_filepath):
-    """Extrai mês, ano e periodicidade do caminho do bucket."""
-    prefix = "tcmbapublicconsultation/"
-    start_index = s3_filepath.find(prefix) + len(prefix)
-    relative_path = Path(s3_filepath[start_index:])
-    folders = relative_path.parts
-    year = folders[0]
-    period = folders[1]
-    month = None
-    if period == "mensal":
-        month = folders[2]
-    return {"year": year, "period": period, "month": month}
 
 
 def build_s3_path(s3_filepath, unit, category, filename):
@@ -55,15 +40,6 @@ def build_s3_url(s3_filepath):
     return urllib.parse.quote_plus(f"{s3_url}{s3_filepath}")
 
 
-def get_original_filename(item):
-    if item.get("original_filename"):
-        return item["original_filename"]
-    else:
-        # exemplo: 1a35fd41-e463-488a-936e-a526d3afa72a-OF\u00cdCIO.pdf
-        uuid4_len = 36
-        return item["filename"][uuid4_len + 1 :]
-
-
 class Command(BaseCommand):
     help = "Importa documentos do TCM-BA em um bucket S3."
 
@@ -82,7 +58,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.echo(f"Caminho no S3: {options.get('s3_path')}")
-        params = extract_params(options.get("s3_path"))
 
         file_items = client.download_file(options.get("s3_path"))
         json_items = json.loads(open(file_items).read())
@@ -94,30 +69,42 @@ class Command(BaseCommand):
             if confirmation.lower() in ["s", "y"]:
                 TCMBADocument.objects.all().delete()
 
+        failed = 0
         for item in json_items:
-            self.echo(str(item))
             s3_file_path = build_s3_path(
                 options.get("s3_path"), item["unit"], item["category"], item["filename"]
             )
-            s3_url = build_s3_url(options.get("s3_path"))
+            s3_url = build_s3_url(s3_file_path)
 
-            document, _ = TCMBADocument.objects.get_or_create(
-                year=params["year"],
-                month=params["month"],
-                period=params["period"],
+            document, created = TCMBADocument.objects.get_or_create(
+                year=item["year"],
+                month=item["month"],
+                period=item["period"],
                 category=item["category"],
                 unit=item["unit"],
                 inserted_at=from_str_to_date(item["inserted_at"]),
                 inserted_by=item["inserted_by"],
-                original_filename=get_original_filename(item),
-                crawled_at=datetime.fromisoformat(item["crawled_at"]),
+                original_filename=item["original_filename"],
                 crawled_from=public_view_url,
+                defaults={
+                    "crawled_at": datetime.fromisoformat(item["crawled_at"]).replace(
+                        tzinfo=timezone.utc
+                    ),
+                },
             )
             content_type = get_content_type_for_model(document)
-            File.objects.get_or_create(
-                url=public_view_url,
-                content_type=content_type,
-                object_id=document.pk,
-                s3_url=s3_url,
-                s3_file_path=s3_file_path,
-            )
+            if created:
+                _, file_created = File.objects.get_or_create(
+                    url=public_view_url,
+                    content_type=content_type,
+                    object_id=document.pk,
+                    s3_url=s3_url,
+                    s3_file_path=s3_file_path,
+                    original_filename=item["original_filename"],
+                )
+                if not file_created:
+                    self.warn(f"Arquivo já existe: {document.pk} - {item}")
+            else:
+                self.warn(f"Documento já existe: {document.pk} - {item}")
+                failed += 1
+        self.warn(f"Warnings: {failed}")
