4bad84ea7a2e385f447cb33eb2afd66257f2b754
Ana Paula Gomes <apgomes88@gmail.com>
2021-04-11 22:14:41 +0200

    Adiciona feature flag para tasks dos arquivos

--- web/datasets/signals.py
+++ web/datasets/signals.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from dramatiq import pipeline
@@ -8,14 +10,15 @@ from .models import File
 @receiver(post_save, sender=File)
 def backup_and_extract_content(sender, instance, **kwargs):
     """Faz backup e extrai conteúdo de um arquivo após sua criação."""
-    from .tasks import backup_file, content_from_file
+    if bool(os.getenv("ENABLE_SIGNAL_FOR_FILE_TASKS", False)):
+        from .tasks import backup_file, content_from_file
 
-    if instance.s3_url is None or instance.content is None:
-        pipeline(
-            [
-                backup_file.message(instance.pk),
-                content_from_file.message_with_options(
-                    pipe_ignore=True, args=(instance.pk,)
-                ),
-            ]
-        ).run()
+        if instance.s3_url is None or instance.content is None:
+            pipeline(
+                [
+                    backup_file.message(instance.pk),
+                    content_from_file.message_with_options(
+                        pipe_ignore=True, args=(instance.pk,)
+                    ),
+                ]
+            ).run()
