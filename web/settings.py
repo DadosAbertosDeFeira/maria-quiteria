9aecbda7094c72eff8fe0fdfdb485b9826dd0138
Edson C <edsoncelio@users.noreply.github.com>
2021-05-02 13:37:48 -0300

    Adiciona arquivo CHECKS  (#312)
    * Adiciona CHECKS file
    
    * Adiciona arquivo de CHECKs usado pelo Dokku para garantir zero downtime deployment.
    * Alterar ALLOWED_HOSTS
    
    * Altera ALLOWED_HOSTS para usar hosts dinâmicos

--- web/settings.py
+++ web/settings.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import timedelta
+from socket import gethostbyname, gethostname
 
 import dj_database_url
 import sentry_sdk
@@ -164,6 +165,7 @@ class Test(Dev):
 
 class Prod(Common):
     SECRET_KEY = values.SecretValue()
-    ALLOWED_HOSTS = values.ListValue()
+    ALLOWED_HOSTS = values.ListValue([], environ_name="ALLOWED_HOSTS")
+    ALLOWED_HOSTS.extend((gethostname(), gethostbyname(gethostname())))
     DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=False)}
     GOOGLE_ANALYTICS_KEY = values.Value()
