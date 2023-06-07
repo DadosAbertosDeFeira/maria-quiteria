5d2f00424d86dc1261e0242314f0eeb4cd40365c
Sheory <69269697+sheory@users.noreply.github.com>
2023-06-07 02:12:16 -0300

    Adiciona documentação da API com drf-yasg (#622)
    * Adiciona swaegger com drf-yasg(278)
    
    * Corrige swagger schema_view params e padrao black no re_path (278)
    
    * Remove cache config (278)

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,6 +15,17 @@ from rest_framework_simplejwt.views import (
 from web.datasets.admin import public_admin
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Maria Quitéria API",
+        default_version="v1",
+        contact=openapi.Contact(email="dadosabertosdefeira+api@gmail.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("web.home.urls")),
@@ -18,6 +34,11 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    re_path(
+        r"^api/docs/$",
+        schema_view.with_ui("swagger"),
+        name="schema-swagger-ui",
+    ),
 ]
 
 
